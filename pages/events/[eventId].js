d731ab7e1362236bffacd0955c8d030ddb45b086
NhungTranPY <nhungtranpy91@gmail.com>
2023-06-21 16:36:11 +0700

    122. Loading Data & Paths For Dynamic Pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -2,16 +2,14 @@ import EventContent from "@/components/event-detail/event-content"
 import EventLogistics from "@/components/event-detail/event-logistics"
 import EventSummary from "@/components/event-detail/event-summary"
 import ErrorAlert from "@/components/ui/error-alert"
-import { getEventById } from "@/dummy-data"
-import { useRouter } from "next/router"
+import { getEventById, getAllEvents } from "../../helpers/api-util"
 
-function EventDetailPage() {
+function EventDetailPage(props) {
 
-    const router = useRouter()
-
-    const eventId = router.query.eventId // eventId nay phai trung voi ten file moi access lay dc id
-
-    const event = getEventById(eventId)
+    // const router = useRouter()
+    // const eventId = router.query.eventId // eventId nay phai trung voi ten file moi access lay dc id
+    // const event = getEventById(eventId)
+    const event = props.selectedEvent
 
     if (!event) {
         return (
@@ -35,6 +33,29 @@ function EventDetailPage() {
             </EventContent>
         </>
     )
+}
+
+export async function getStaticProps(context) {
+    const {eventId} = context.params.eventId
+
+    const event = await getEventById(eventId)
+
+    return {
+        props: {
+            selectedEvent: event
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const events = await getAllEvents()
+
+    const paths = events.map(event => ({ params: {eventId: event.id}}))
+
+    return {
+        paths: paths,
+        fallback: false // to let Nextjs knows if load this file for unknown id then render 404
+    }
 }
 
 export default EventDetailPage
