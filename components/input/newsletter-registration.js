7debe4774483555d09c51b20f660771a8e5d6c20
NhungTranPY <nhungtranpy91@gmail.com>
2023-06-28 16:23:12 +0700

    156. Adding a Newsletter Route

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,12 +1,29 @@
+import { useRef } from 'react';
 import classes from './newsletter-registration.module.css';
+import { headers } from 'next/dist/client/components/headers';
 
 function NewsletterRegistration() {
+
+  const emailInputRef = useRef()
+
   function registrationHandler(event) {
     event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value
 
     // fetch user input (state or refs)
     // optional: validate input
     // send valid data to API
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify({email: enteredEmail}), // convert data to JSON, 'email' because in newsletter api we extract req.body.email
+      headers: {
+        'Content-Type': 'application/json',
+
+      }
+    })
+      .then((response) => response.json)
+      .then((data) => console.log(data))
   }
 
   return (
@@ -19,6 +36,7 @@ function NewsletterRegistration() {
             id='email'
             placeholder='Your email'
             aria-label='Your email'
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
