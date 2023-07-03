4accfca3eb10be4be6db5023955718248ebd8ce1
NhungTranPY <nhungtranpy91@gmail.com>
2023-07-03 11:58:01 +0700

    173. Using Context Data In Components

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,19 @@
 import Head from 'next/head'
 import Layout from '@/components/layout/layout'
-import Notification from '@/components/ui/notification'
 import '@/styles/globals.css'
 import { NotificationContextProvider } from '@/store/notification-context'
 
 export default function App({ Component, pageProps }) {
   return (
     <NotificationContextProvider>
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-       <Component {...pageProps} />
-       <Notification title="Test" message="This is a test." status="pending" />
-    </Layout>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name='description' content='NextJS Events' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
     </NotificationContextProvider>
   )
 }
